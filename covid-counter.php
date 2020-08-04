ea1bb9decfd071a38152fab53a2c9b04374a7c4d
Jared Cleghorn <jrcleghorn@crimson.ua.edu>
2020-08-04 17:14:08 -0500

    Add count retrieval

--- covid-counter.php
+++ covid-counter.php
@@ -8,10 +8,11 @@
 
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
-register_activation_hook( __FILE__, function() {
-	global $wpdb;
+global $wpdb;
 
-	$table_name = $wpdb->prefix . 'covid_counter_movements';
+$table_name = $wpdb->prefix . 'covid_counter_movements';
+
+register_activation_hook( __FILE__, function() use ( $wpdb, $table_name ) {
 	$charset_collate = $wpdb->get_charset_collate();
 
 	dbDelta( "
@@ -22,5 +23,21 @@ register_activation_hook( __FILE__, function() {
 			PRIMARY KEY (id)
 		) $charset_collate;
 	" );
+} );
+
+add_action( 'rest_api_init', function() use ( $wpdb, $table_name ) {
+	register_rest_route( 'covid-counter', '/count', array(
+		'methods'  => 'GET',
+		'callback' => function() use ( $wpdb, $table_name ) {
+			return array( 'count' => (int) $wpdb->get_var( "
+				SELECT SUM(
+					CASE
+						WHEN type = 'entry' THEN 1
+						WHEN type = 'exit' THEN -1
+					END
+				) FROM $table_name
+			" ) ?? 0 );
+		},
+	) );
 } );
 ?>
