69d1baf400f1b0e7250084498adb2f5e1b3ba322
Jared Cleghorn <jrcleghorn@crimson.ua.edu>
2020-08-25 17:00:36 -0500

    Fix type validation

--- functions/validate_type.php
+++ functions/validate_type.php
@@ -2,6 +2,7 @@
 require_once( plugin_dir_path( __FILE__ ) . '../constants.php' );
 
 function validate_type( $type ) {
-	return in_array( $type, $movement_types );
+	global $types;
+	return in_array( $type, $types );
 }
 ?>
